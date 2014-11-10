43a5d4ebeda02f7d9c0e6b656e27ee9ac8c60868
andsonye <andsonye@andsonye-Aspire-V7-582PG.(none)>
2014-11-10 11:26:45 +0800

    add files

---
+++ firmware/mylib.h
@@ -0,0 +1,17 @@
+#ifndef Emic2TtsModule_h
+#define Emic2TtsModule_h
+
+#include "application.h"
+
+enum EmicVoice {
+    PerfectPaul = 0,
+    HugeHarry = 1,
+    BeautifulBetty = 2,
+    UppityUrsula = 3,
+    DoctorDennis = 4,
+    KitTheKid = 5,
+    FrailFrank = 6,
+    RoughRita = 7,
+    WhisperingWendy = 8
+};
+
